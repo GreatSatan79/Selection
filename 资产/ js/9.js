b8e85a33c3d67e79fdc8ee29fb1a0790786883fd
79 <1962257451@qq.com>
2019-06-24 21:19:34 +0800

    Update and rename assets/js/8.js to 资产/ js/9.js

--- 资产/ js/9.js
+++ 资产/ js/9.js
@@ -2,8 +2,8 @@ $("img[src='http://www.kuaimitu.com/static/upload/af4a462034ad73d6/e44de4c031d06
 var file = 'https://fonts.googleapis.com/css?family=Kosugi+Maru|ZCOOL+KuaiLe'; 
 
 document.getElementsByTagName("title")[0].innerText = '次元发卡 - 赛高次元';
-document.write('<style type=\\"text/css\\">@import \\"' + file + '\\" ;</style>');
-document.write('<style type=\\"text/css\\">.dianpu{font-family:ZCOOL KuaiLe,cursive;}.nav li:nth-child(2){display: none;}</style>');
+document.write('<style type="text/css">@import \\"' + file + '\\" ;</style>');
+document.write('<style type="text/css">.dianpu{font-family:ZCOOL KuaiLe,cursive;}.nav li:nth-child(2){display: none;}</style>');
 (function() {
   $('.layui-layer-shade').css('display','none');
   $('.layui-layer').css('display','none');
