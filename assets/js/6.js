d50543c1877140cb8a04ac21f9e710905eca555d
79 <1962257451@qq.com>
2019-06-24 19:39:54 +0800

    Update and rename 5.js to 6.js

--- assets/js/6.js
+++ assets/js/6.js
@@ -1,8 +1,12 @@
 $("img[src='http://www.kuaimitu.com/static/upload/af4a462034ad73d6/e44de4c031d06be4.png']").attr('src','http://i2.tiimg.com/568907/4e869d831e321e91.png');
+var file = 'https://fonts.googleapis.com/css?family=Kosugi+Maru|ZCOOL+KuaiLe'; 
 document.getElementsByTagName("title")[0].innerText = '次元发卡 - 赛高次元';
+document.write('<style type=\\"text/css\\">@import \\"' + file + '\\" ;</style>');
 (function() {
   $('.layui-layer-shade').css('display','none');
   $('.layui-layer').css('display','none');
   $('.ewm').css('display','none');
   $('.spsm').css('display','none');
+  $('.dianpu').css('font-family','ZCOOL KuaiLe,cursive');
 })();
+
