6701768d8d0d11dedc4294e66c5cdd491be1ce2f
79 <1962257451@qq.com>
2019-06-24 19:22:09 +0800

    Update kz.js

--- assets/js/kz.js
+++ assets/js/kz.js
@@ -1,1 +1,5 @@
 $("img[src='http://www.kuaimitu.com/static/upload/af4a462034ad73d6/e44de4c031d06be4.png']").attr('src','http://i2.tiimg.com/568907/4e869d831e321e91.png');
+
+(function() {
+  $('.layui-layer-shade').css('display','none');
+})();
